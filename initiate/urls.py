87b20243f5dc6f31f6efb939884baee1d95e4969
amogh.saxena <welcome2@digivalet>
2022-06-13 19:05:11 +0530

    Backup!

---
+++ initiate/urls.py
@@ -0,0 +1,19 @@
+from . import views
+from django.urls import path
+from django.contrib import admin
+from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from .views import RegisterView
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('createticker', views.createticker, name = 'createticker'),
+    path('active', views.active, name = 'active'),
+    path('pending', views.pending, name = 'pending'),
+    path('history', views.history, name = 'history'),
+    path('preview', views.preview, name = 'preview'),
+    path('schedule', views.schedule, name = 'schedule'),
+    path('registerform',views.registerfor,name='registerform'),
+    path('register',RegisterView.as_view(), name='register'),
+    # url(r'^login/$', login, {'template_name': 'admin/login.html'})
+    path('login', views.login, name = 'login')
+]
---
+++ static/js/createticker_enabler.js
@@ -0,0 +1,156 @@
+function fostaticEnabler()
+{
+    document.getElementById('primary_form').hidden=true
+    document.getElementById('secondary_form').hidden=true
+    document.getElementById('animation_form').hidden=true
+    document.getElementById('emergency_form').hidden=true
+    document.getElementById('hello1').hidden=true
+    document.getElementById('hello2').hidden=true
+    document.getElementById('hello3').hidden=true
+    document.getElementById('hello4').hidden=true
+    document.getElementById('hello5').hidden=true
+    document.getElementById('animation_text').hidden=true
+    document.getElementById('primary_file_obj').hidden=true
+    document.getElementById('primary_logo_pos').hidden=true
+    document.getElementById('static_logo').hidden=true
+}
+
+function fosubmit(id)
+{
+	if (id=='staticticker')
+    { 	
+    	document.getElementById('static_form').hidden=false
+    	document.getElementById('primary_form').hidden=true
+    	document.getElementById('secondary_form').hidden=true
+    	document.getElementById('animation_form').hidden=true
+    	document.getElementById('emergency_form').hidden=true
+    }
+    if (id=='primaryticker')
+    {
+    	document.getElementById('static_form').hidden=true
+    	document.getElementById('primary_form').hidden=false
+    	document.getElementById('secondary_form').hidden=true
+    	document.getElementById('animation_form').hidden=true
+    	document.getElementById('emergency_form').hidden=true
+    }
+    if (id=='secondaryticker')
+    {
+    	document.getElementById('static_form').hidden=true
+    	document.getElementById('primary_form').hidden=true
+    	document.getElementById('secondary_form').hidden=false
+    	document.getElementById('animation_form').hidden=true
+    	document.getElementById('emergency_form').hidden=true
+    }
+    if (id=='animationticker')
+    {
+    	document.getElementById('static_form').hidden=true
+    	document.getElementById('primary_form').hidden=true
+    	document.getElementById('secondary_form').hidden=true
+    	document.getElementById('animation_form').hidden=false
+    	document.getElementById('emergency_form').hidden=true
+    }
+    if (id=='emergencyticker')
+    {
+    	document.getElementById('static_form').hidden=true
+    	document.getElementById('primary_form').hidden=true
+    	document.getElementById('secondary_form').hidden=true
+    	document.getElementById('animation_form').hidden=true
+    	document.getElementById('emergency_form').hidden=false
+    }
+}
+
+function foeventEnabler(id)
+{
+	if (id=='static_form_data')
+    {
+        var b=document.getElementById('static_form_data')
+    	if (b.checked)
+        {document.getElementById('hello1').hidden=false}
+        else
+        {document.getElementById('hello1').hidden=true}
+    }
+    if (id=='primary_form_data')
+    {
+        var b=document.getElementById('primary_form_data')
+    	if (b.checked)
+        {document.getElementById('hello2').hidden=false}
+        else
+        {document.getElementById('hello2').hidden=true}
+    }
+    if (id=='secondary_form_data')
+    {
+        var b=document.getElementById('secondary_form_data')
+    	if (b.checked)
+        {document.getElementById('hello3').hidden=false}
+        else
+        {document.getElementById('hello3').hidden=true}
+    }
+    if (id=='animation_form_data')
+    {
+        var b=document.getElementById('animation_form_data')
+    	if (b.checked)
+        {document.getElementById('hello4').hidden=false}
+        else
+        {document.getElementById('hello4').hidden=true}
+    }
+    if (id=='emergency_form_data')
+    {
+        var b=document.getElementById('emergency_form_data')
+    	if (b.checked)
+        {document.getElementById('hello5').hidden=false}
+        else
+        {document.getElementById('hello5').hidden=true}
+    }
+
+    if (id=='checkbox_for_static_logo')
+    {
+        var b=document.getElementById('checkbox_for_static_logo')
+    	if (b.checked)
+        {document.getElementById('static_logo').hidden=false}
+        else
+        {document.getElementById('static_logo').hidden=true}
+    }
+
+    if(id=='animation_pos')
+    {
+        var select = document.getElementById('animation_pos');
+		var option = select.options[select.selectedIndex];
+
+		if(option.text=='center')
+        {
+            document.getElementById('animation_text').hidden=false
+        }
+        else
+        {
+            document.getElementById('animation_text').hidden=true
+        }
+        
+	}
+    if(id=='primary_logo_enabler')
+    {
+        var b=document.getElementById('primary_logo_enabler')
+    	if (b.checked)
+        {
+            document.getElementById('primary_file_obj').hidden=false
+            document.getElementById('primary_logo_pos').hidden=false
+        }
+        else
+        {
+            document.getElementById('primary_file_obj').hidden=true
+            document.getElementById('primary_logo_pos').hidden=true
+        }
+    }
+
+    if(id=='checkbox_for_static_logo')
+    {
+        var b=document.getElementById('checkbox_for_static_logo')
+        if(b.checked)
+        {
+            document.getElementById('static_logo').hidden=false
+        }
+        else
+        {
+            document.getElementById('static_logo').hidden=true
+        }
+    }
+}
