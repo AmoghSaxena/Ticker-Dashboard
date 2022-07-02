0f9eb876434cb1726a65ebb602820c1c25d581e9
theraxstar <rohitpatelworkingid@gmail.com>
2022-07-02 12:32:25 +0530

    New changes for templates

--- initiate/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from initiate.models import TickerDetails,TickerHistory
-
-class AdminTable(admin.ModelAdmin):
-    pass
-admin.site.register(TickerDetails,AdminTable)
-admin.site.register(TickerHistory,AdminTable)
