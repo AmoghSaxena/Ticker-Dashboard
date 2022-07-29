df42567bbeb45f2fcfe1a02f5dbaa5883a20e4c5
theraxstar <rohitpatelworkingid@gmail.com>
2022-07-29 19:22:25 +0530

    Chnages in Compose file

--- compose/production/mariadb/setup.sql
+++ compose/production/mariadb/setup.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS 'Ticker';
-CREATE USER IF NOT EXISTS 'Ticker'@'%' identified by '92b9a3b730';
+-- CREATE DATABASE IF NOT EXISTS 'Ticker';
+-- CREATE USER IF NOT EXISTS 'Ticker'@'%' identified by '92b9a3b730';
 CREATE USER IF NOT EXISTS 'rohit'@'192.168.%' identified by 'admin1234';
-GRANT ALL PRIVILEGES ON Ticker.* TO rohit@'192.168.%';
+GRANT ALL PRIVILEGES ON *.* TO rohit@'192.168.%';
